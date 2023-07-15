d6d154b0cb213763e491ebd4acd0508db73ed942
김주현 <freentour@gmail.com>
2023-07-15 13:44:51 +0900

    settings.prod STATIC_ROOT 마지막 '/' 빼먹은거 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 ALLOWED_HOSTS = ['3rdx.2woo.net']
 DEBUG = False
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 # allauth를 위한 SITE_ID
