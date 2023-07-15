b6c5ab509017337839592ac25c65c0f81b19a0c6
김주현 <freentour@gmail.com>
2023-07-15 19:48:25 +0900

    pybo 로거 추가하고 강제로 로깅 발생시켜 봄

--- config/settings/base.py
+++ config/settings/base.py
@@ -250,5 +250,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
