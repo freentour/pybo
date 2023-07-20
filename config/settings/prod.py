d5fead6403c43a49890cd3bca18ac183ebf896b7
김주현 <freentour@gmail.com>
2023-07-20 09:24:58 +0900

    settings.prod에 MySQL 데이터베이스 연결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,10 +1,29 @@
+import environ
 from .base import *
 
 ALLOWED_HOSTS = ['3rdx.2woo.net']
 DEBUG = False
 
+env = environ.Env()
+env.read_env(BASE_DIR / '.env')
+
+
+# MySQL Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT' : '3306',
+    }
+}
+
+
 # STATIC_ROOT = BASE_DIR / 'static/'
 # STATICFILES_DIRS = []
+
 
 # allauth를 위한 SITE_ID
 SITE_ID = 3
