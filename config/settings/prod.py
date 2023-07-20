6cd825fc0c00ca9fd4336bf01e6c41ed76d9000e
김주현 <freentour@gmail.com>
2023-07-20 18:39:59 +0900

    settings.prod PostgreSQL ENGINE값 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -26,7 +26,7 @@ DATABASES = {
 # PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
